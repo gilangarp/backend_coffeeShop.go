262cbce1375d64da32aefcd9047804d3b49b479f
gilangarp <gilang2001.ap@gmail.com>
2024-08-14 12:58:18 +0700

    add Cloudinary upload functionality

--- internal/models/profile.models.go
+++ internal/models/profile.models.go
@@ -5,7 +5,7 @@ type Profile struct {
 	Display_name     string `db:"display_name" json:"display_name" form:"display_name" `
 	First_name       string `db:"first_name" json:"first_name" form:"first_name"`
 	Last_name        string `db:"last_name" json:"last_name" form:"last_name" `
-	Image            string `db:"image" json:"image" form:"image" `
+	Image            string `db:"image" json:"image" `
 	Delivery_address string `db:"delivery_address" json:"delivery_address" form:"delivery_address" `
 	Birth_date       string `db:"birth_date" json:"birth_date" form:"birth_date" `
 }
